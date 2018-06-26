e43ee7f90bb9aa503b72a2027e97932efa9de9d3
Luna <lunakticeu@hotmail.com>
2018-06-26 20:04:42 -0300

    create myDriver

--- src/test/java/drivers/MyDriver.java
+++ src/test/java/drivers/MyDriver.java
@@ -1,18 +1,15 @@
-import java.util.concurrent.TimeUnit;
+package drivers;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Driver {
+import java.util.concurrent.TimeUnit;
 
-   // public static WebDriver driver = null;
+public class MyDriver {
     public static WebDriver driver;
 
     public void initialize() {
-
         System.setProperty("webdriver.chrome.driver", "/home/luna/Documents/ProjetoAutomacaoJava/chromedriver");
-
-
-        driver = new ChromeDriver();
+        driver = new org.openqa.selenium.chrome.ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
